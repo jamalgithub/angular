837d24e247b4815b5a617e1dd4e89b9a0af387e7
jamalgithub <jelmoubarik@gmail.com>
2018-04-06 15:42:33 +0000

    06/04/2018 at 15:42

---
+++ my-app-11/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+    constructor(private router: Router) {
+    }
+
+    ngOnInit() {
+    }
+
+    onLoadServer(id: number) {
+        // http://localhost:4200/servers/1/edit?allowEdit=1#loading
+        this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: '1'}, fragment: 'loading'});
+
+        // http://localhost:4200/servers?param1=value1&param2=value2
+        // this.router.navigate(['/servers'], { queryParams: { param1: 'value1', 'param2': 'value2' } });
+
+        // this.router.navigate(['/servers']).then(
+        //     nav => {
+        //         // true if navigation is successful
+        //         console.log(nav);
+        //     },
+        //     err => {
+        //         // when there's an error
+        //         console.log(err);
+        //     }
+        // );
+    }
+}
