c0c10b5190c8a21a3a8a166be30e03703852bc2d
jamalgithub <jelmoubarik@gmail.com>
2018-04-20 14:25:10 +0000

    20/04/2018 at 14:24

---
+++ my-app-12/app/app.component.ts
@@ -0,0 +1,60 @@
+import {Component} from '@angular/core';
+import {ServerObject} from './shared/server-object';
+import {ElementObject} from './shared/element-object';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    loadedFeature = 'recipe';
+
+    serverElements: ElementObject[] = [
+        // {
+        //     type: 'server',
+        //     name: 'TestServer',
+        //     content: 'Just a test!'
+        // }
+        new ElementObject('server', 'TestServer', 'Just a test!')
+    ];
+
+    onRemoveServer(id: number) {
+        const position = id;
+        // this.servers.splice(position, 1);
+    }
+
+    onServerAdded(serverData: ServerObject) {
+        this.serverElements.push(
+            // {
+            //     type: 'server',
+            //     name: serverData.serverName,
+            //     content: serverData.serverContent
+            // }
+            new ElementObject('server', serverData.serverName, serverData.serverContent)
+        );
+    }
+
+    onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
+        this.serverElements.push(
+            // {
+            //     type: 'blueprint',
+            //     name: blueprintData.serverName,
+            //     content: blueprintData.serverContent
+            // }
+            new ElementObject('blueprint', blueprintData.serverName, blueprintData.serverContent)
+        );
+    }
+
+    onChangeFirst() {
+        this.serverElements[0].name = 'Changed!';
+    }
+
+    onDestroyFirst() {
+        this.serverElements.splice(0, 1);
+    }
+
+    onNavigate(feature: string) {
+        this.loadedFeature = feature;
+    }
+}
